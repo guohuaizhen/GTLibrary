f4c3f9861b96958c264920b5e19c9869635634df
guohuaizhen <15066277652@163.com>
2019-03-24 17:29:37 +0800

    0.1.2

---
+++ Controller/YCRootViewController.h
@@ -0,0 +1,16 @@
+//
+//  YCRootViewController.h
+//  GTLibrary
+//
+//  Created by 淡泊明志 宁静致远 on 2019/3/24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YCRootViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Controller/YCRootViewController.m
@@ -0,0 +1,31 @@
+//
+//  YCRootViewController.m
+//  GTLibrary
+//
+//  Created by 淡泊明志 宁静致远 on 2019/3/24.
+//
+
+#import "YCRootViewController.h"
+
+@interface YCRootViewController ()
+
+@end
+
+@implementation YCRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
